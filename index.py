79baf838bd6404e59e6adde5a6ca573279ee43d4
Caely <rdr@cesar.school>
2020-09-04 20:38:20 -0300

    task-001: code developing.

--- index.py
+++ index.py
@@ -3,7 +3,9 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from time import sleep
 import json
 
 def get_product_urls(driver):
@@ -13,30 +15,42 @@ def get_product_urls(driver):
         EC.presence_of_element_located((By.CLASS_NAME, 'app__catalog__grid'))
     )
     products = catalog_grid.find_elements_by_class_name('app__product-box')
-    return map(
+    return list(map(
         lambda p: p.find_element_by_tag_name('a').get_attribute("href"),
         products
-    )
+    ))
 
 def get_review_data(driver):
-    driver.execute_script("window.scrollTo(0, 1500)") 
-    container = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'yotpo-star-distribution-content'))
-    )
-    stars = container.find_elements_by_class_name(
-        'review-stars'
-    )
-
     # Get reviews from 1,2,3 stars
-    for i in (2, 3, 4):
-        stars[i].click()
-        container = WebDriverWait(driver, 10).until(
+    for i in (1, 2, 3):
+        star = function(driver, i)
+        star.click()
+        sleep(5)
+        container = WebDriverWait(driver, 60).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'yotpo-reviews'))
         )
-        reviews = container.find_elements_by_class_name('yotpo-review')
+        reviews = container.find_elements(By.XPATH, "//div[@class='yotpo-review-wrapper']/div[@class='content-review']")
+        #TODO verificar se existe comentarios
         for r in reviews:
-            print(r.find_element_by_class_name('content-title').text)
-        break
+            print(r.text)
+
+
+def function(driver, i):
+    try:
+        WebDriverWait(driver,20).until(
+            EC.presence_of_element_clickable(
+                (By.XPATH,
+                f"//span[@data-score-distribution='{i}' and contains(@class,'review-stars')]")
+            )
+        )
+    except:
+        ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+    finally:
+        star = driver.find_element(
+            *(By.XPATH,
+            f"//span[@data-score-distribution='{i}' and contains(@class,'review-stars')]")
+        )
+        return star
 
 
 # -- MAIN -- #
